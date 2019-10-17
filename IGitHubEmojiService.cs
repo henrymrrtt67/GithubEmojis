7623d24270cf8d0dfde37cd3bcc7f20faebd9889
Henry Merritt <henrymerritt@Henrys-MacBook-Pro-2.local>
2019-10-17 19:04:41 +1300

    The total tutorial, will see if I can make it so that the UrLocal can go from here

---
+++ IGitHubEmojiService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GithubEmojis
+{
+    public interface IGitHubEmojiService
+    {
+        Task<IList<Emoji>> GetEmojis();
+        IList<Emoji> GetEmojisFrom(string content);
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace GithubEmojis.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private IGitHubEmojiService _emogiService;
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(IGitHubEmojiService emojiSvc)
+        {
+            _emogiService = emojiSvc;
+        }
+        public IList<Emoji> Emojis
+        {
+            get;
+            set;
+        }
+        public async Task OnGet()
+        {
+            Emojis = await _emogiService.GetEmojis();
+
+        }
+    }
+}
